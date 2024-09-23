eb1c9f8964d8db2fd940724aa1f66c4be2e25d80
Mutaharaslam <mutahar@medlucy.com>
2024-09-24 04:10:02 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/home";
-import ContactUs from "./pages/contact";
+import Contact from "./pages/contact";
+import Header from "./components/header/header";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="App bg-secondary">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/contact" element={<ContactUs />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 // src/pages/homepage.tsx
 
 import React from 'react';
-import Header from '../components/header/header';
 import Banner from '../components/banner/banner';
 import RoadMap from '../components/roadmap/roadmap';
 import RewardCounter from '../components/reward-counter/reward-counter';
@@ -9,7 +8,6 @@ import RewardCounter from '../components/reward-counter/reward-counter';
 const Homepage: React.FC = () => {
   return (
     <div className="Homepage bg-secondary h-full flex flex-col md:px-10 px-6">
-      <Header />
       <Banner />
       <RoadMap />
       <RewardCounter />
