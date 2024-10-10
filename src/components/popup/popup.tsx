16e14368e67fac98ac580ffb72fa843b6a88fc06
Mutaharaslam <mutahar@medlucy.com>
2024-10-10 15:05:45 +0500

    fix

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -8,13 +8,13 @@ interface popupIprops {
   onClose: () => void;
 }
 
-const Popup: React.FC<popupIprops> = ({ isOpen, status, onClose }) => {
+const Popup: React.FC<popupIprops> = ({ isOpen, status = '', onClose }) => {
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-950 bg-opacity-70">
       <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center relative">
         {/* Modal Header with Close Button */}
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center mb-4 border-b pb-4">
           <h2 className="text-xl font-semibold text-secondary">
             Minting Status
           </h2>
@@ -26,7 +26,12 @@ const Popup: React.FC<popupIprops> = ({ isOpen, status, onClose }) => {
           </button>
         </div>
 
-        <div className=" flex flex-col justify-start items-center py-16">
+        <div className="flex flex-col justify-start items-center pt-10 pb-16">
+          {!status.toLowerCase().includes("error") && (
+            <h3 className="text-secondary mb-4">
+              Please confirm transaction from your wallet.
+            </h3>
+          )}
           <Loader className="w-24 h-24 mb-4" />
 
           {/* Status Text */}
